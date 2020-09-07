2fbdb732d197ff74e3ad701a37a3d0a2592dcfd7
Krystian Fluder <krystian.fluder@outlook.com>
2020-09-07 10:44:58 +0200

    Updates

---
+++ precache-manifest.0a0e4bf8cf5e47cdf24c8a27ef2bc6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ef6cbff8437ba39758e98b2f1e5381",
+    "url": "/currencies/index.html"
+  },
+  {
+    "revision": "9a3c21bfcb8196ff2ce9",
+    "url": "/currencies/static/css/main.52b4ce09.chunk.css"
+  },
+  {
+    "revision": "bf68266622dd5784e92a",
+    "url": "/currencies/static/js/2.51bc83fd.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/currencies/static/js/2.51bc83fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3c21bfcb8196ff2ce9",
+    "url": "/currencies/static/js/main.09202c7a.chunk.js"
+  },
+  {
+    "revision": "734d082980a46178d555",
+    "url": "/currencies/static/js/runtime-main.28b2b8fd.js"
+  }
+]);
